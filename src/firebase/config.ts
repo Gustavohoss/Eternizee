c06691ebf0e10bb21af7cc3f13ac8e3d01f96a91
HOSS STORE <storehoss1@gmail.com>
2026-05-01 10:55:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "eternize-11993828-df02b",
+  "appId": "1:93458160738:web:eee37b8a4031242f37053b",
+  "apiKey": "AIzaSyDhg6CtZOaWdMqR6mxusL0eWPk6z4S3tS8",
+  "authDomain": "eternize-11993828-df02b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "93458160738"
+};
